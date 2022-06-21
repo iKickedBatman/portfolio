0d02ffca79f825c34fa9c18e6711f4be583b19f7
iKickedBatman <Dan_S3@outlook.com>
2022-06-22 00:41:10 +0100

    footer done

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+// let coll = document.getElementsByClassName("btn");
+// let i;
+
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     let content = this.nextElementSibling;
+//     if (content.style.maxHeight){
+//       content.style.maxHeight = null;
+//     } else {
+//       content.style.maxHeight = content.scrollHeight + "px";
+//     } 
+//   });
+// }
+
+// let btn = document.createElement("cv-btn")
+// btn.innerText = "View CV"
+// document.body.appendChild(btn)
+// btn.addEventListener("click", viewCV)
+
+
+// function viewCV(src, width, height, alt) {
+//   let btn = document.createElement("cv-btn")
+//   btn.src = src;
+//   btn.width = width;
+//   btn.height = height;
+//   btn.alt = alt;
+//   document.body.appendChild(btn)
+// }
+
+// viewCV('DS_CV.png', 800, 600, 'CV')
