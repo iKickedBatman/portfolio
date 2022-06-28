6aba3a6d53c61ffc60e86a3d28fb5959b5c11c1f
iKickedBatman <Dan_S3@outlook.com>
2022-06-28 11:12:01 +0100

    added neon border to bottom of footer

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,33 @@
-// let coll = document.getElementsByClassName("btn");
-// let i;
+let coll = document.getElementsByClassName("btn");
+let i;
 
-// for (i = 0; i < coll.length; i++) {
-//   coll[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     let content = this.nextElementSibling;
-//     if (content.style.maxHeight){
-//       content.style.maxHeight = null;
-//     } else {
-//       content.style.maxHeight = content.scrollHeight + "px";
-//     } 
-//   });
-// }
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
 
-// let btn = document.createElement("cv-btn")
+
+let btn = document.getElementById("cv-btn")
 // btn.innerText = "View CV"
-// document.body.appendChild(btn)
-// btn.addEventListener("click", viewCV)
+document.body.appendChild(btn)
+btn.addEventListener("click", viewCV)
 
 
-// function viewCV(src, width, height, alt) {
-//   let btn = document.createElement("cv-btn")
-//   btn.src = src;
-//   btn.width = width;
-//   btn.height = height;
-//   btn.alt = alt;
-//   document.body.appendChild(btn)
-// }
+function viewCV(src= "DS_CV.png",width = 800, height = 600, alt= "CV") {
+  // let btn = document.getElementById("cv-btn")
+  btn.innerText = "View CV"
+  btn.src = src;
+  btn.width = width;
+  btn.height = height;
+  btn.alt = alt;
+  document.body.appendChild(btn)
+}
 
-// viewCV('DS_CV.png', 800, 600, 'CV')
+viewCV()
